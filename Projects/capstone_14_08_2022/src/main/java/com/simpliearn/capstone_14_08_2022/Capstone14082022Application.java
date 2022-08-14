eb86d88e6119a6cf0f2c54cd416f626e9fa8e523
Raghavendra M J <raghavendramj123@gmail.com>
2022-08-14 20:35:36 +0530

    Messed up

---
+++ Projects/capstone_14_08_2022/src/main/java/com/simpliearn/capstone_14_08_2022/Capstone14082022Application.java
@@ -0,0 +1,13 @@
+package com.simpliearn.capstone_14_08_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone14082022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capstone14082022Application.class, args);
+	}
+
+}
